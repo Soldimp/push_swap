19e7859eec19c68f703c5b0fc5f6f11292741a9c
Nuno Manuel Garcia Guerreiro <nugarcia@c2r3s3.42lisboa.com>
2023-01-24 12:54:05 +0000

    push start

---
+++ push_swap.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nugarcia < nugarcia@student.42lisboa.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/24 11:24:06 by nugarcia          #+#    #+#             */
+/*   Updated: 2023/01/24 12:01:03 by nugarcia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+typedef struct s_list
+{
+	struct s_list	*a;
+	struct s_list	*b;
+}	t_list;
+
+
+#endif
+
